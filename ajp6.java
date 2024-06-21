ddc5c054ac8eb7fc7f575ecf4af4d4adf9a37a39
rohtelah10 <rohtelah10@gmail.com>
2024-06-21 11:53:24 +0530

    third commit

--- ajp6.java
+++ ajp6.java
@@ -35,3 +35,26 @@ class frequency{
     }
 }
 
+class reverseNo{
+    int checkDigit(int no){
+        int noOfDigit=0;
+        while(no>0){
+            noOfDigit++;
+            no/=10;
+        }
+        return noOfDigit;
+    }
+    int revNo(int no){
+        int num;
+        int revNo=0;
+        int noOfDigit =  checkDigit(no);
+        while(noOfDigit>=0){
+            num=no%10;
+            no/=10;
+            noOfDigit--;
+            revNo += num*(int)(Math.pow(10, noOfDigit));
+        }
+        return revNo;
+    }
+} 
+
